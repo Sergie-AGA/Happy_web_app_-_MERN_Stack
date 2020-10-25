e650fbff95b1a77266cff697a3e5abf0a14fbedb
Sergie <sergiearruda@gmail.com>
2020-10-25 17:34:20 +0000

    Initial commit
    This initial commit contains all files necessary for the base functioning of the app, with only the mobile implementation missing. Future commits will present new customized features.

---
+++ backend/src/server.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import path from 'path';
+import './database/connection';
+import routes from './routes';
+import 'express-async-errors';
+import errorHandler from './errors/handler';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
+app.use(errorHandler);
+
+// app.get('/users', () => {console.log('test')})
+
+app.listen(3333);
